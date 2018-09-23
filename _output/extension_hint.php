3ce59542982618c732158d7a1e61ee3ecc418334
Lukas Wieditz <lukasw@audentio.com>
2018-09-23 15:03:23 +0200

    Updated .gitignore

---
+++ _output/extension_hint.php
@@ -0,0 +1,53 @@
+<?php
+
+// ################## THIS IS A GENERATED FILE ##################
+// DO NOT EDIT DIRECTLY. EDIT THE CLASS EXTENSIONS IN THE CONTROL PANEL.
+
+namespace KL\EditorManager\Repository
+{
+	class XFCP_SiropuChatData extends \Siropu\Chat\Data {}
+}
+
+namespace KL\EditorManager\XF\Admin\Controller
+{
+	class XFCP_Log extends \XF\Admin\Controller\Log {}
+}
+
+namespace KL\EditorManager\XF\BbCode\Renderer
+{
+	class XFCP_EditorHtml extends \XF\BbCode\Renderer\EditorHtml {}
+	class XFCP_Html extends \XF\BbCode\Renderer\Html {}
+}
+
+namespace KL\EditorManager\XF\BbCode
+{
+	class XFCP_RuleSet extends \XF\BbCode\RuleSet {}
+}
+
+namespace KL\EditorManager\XF\Html\Renderer
+{
+	class XFCP_BbCode extends \XF\Html\Renderer\BbCode {}
+}
+
+namespace KL\EditorManager\XF\Proxy
+{
+	class XFCP_Controller extends \XF\Proxy\Controller {}
+}
+
+namespace KL\EditorManager\XF\Pub\Controller
+{
+	class XFCP_Account extends \XF\Pub\Controller\Account {}
+	class XFCP_Editor extends \XF\Pub\Controller\Editor {}
+	class XFCP_Post extends \XF\Pub\Controller\Post {}
+	class XFCP_Thread extends \XF\Pub\Controller\Thread {}
+}
+
+namespace KL\EditorManager\XF\Str
+{
+	class XFCP_Formatter extends \XF\Str\Formatter {}
+}
+
+namespace KL\EditorManager\XF\SubContainer
+{
+	class XFCP_Proxy extends \XF\SubContainer\Proxy {}
+}
